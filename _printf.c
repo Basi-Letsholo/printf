01c1c304138204adb31a1ffa590fe69882502abb
LionHardy <leonardndobe@gmail.com>
2023-03-26 05:47:52 -0700

    _print.c

--- _printf.c
+++ _printf.c
@@ -9,36 +9,46 @@
 
 int _printf(const char *format, ...)
 {
-	char c;
-	char str[100];
-	int i = 0, j, r;
 
 	va_list args;
 
 	va_start(args, format);
-	if (format[i] == '%')
-	{
-		c = format[i + 1];
-	}
-	switch (c)
-	{
-		case 'c':
-			str[0] = va_arg(args, int);
-			_putchar (str[0]);
-			r = 1;
-			break;
-		case 's':
-			
-			str = va_arg(args, int);
-			for (j = 0; *str != '\0'; j++)
-			{
-				str[j] = va_arg(args, int);
-				_putchar(str[j]);
-			}
-			r = j;
-			break;
-		default:
-			return (0);
-	}
-	return (r);
+	vprintf(format, args);
+	va_end(args);
+
+	return (0);
 }
+
+/*      char c;
+        char str[100];
+        int i = 0, j, r;
+
+        va_list args;
+
+        va_start(args, format);
+        if (format[i] == '%')
+        {
+                c = format[i + 1];
+        }
+        switch (c)
+        {
+                case 'c':
+                        str[0] = va_arg(args, int);
+                        _putchar (str[0]);
+                        r = 1;
+                        break;
+                case 's':
+
+                str = va_arg(args, int);
+                        for (j = 0; *str != '\0'; j++)
+                        {
+                                str[j] = va_arg(args, int);
+                                _putchar(str[j]);
+                        }
+                        r = j;
+                        break;
+                default:
+                        return (0);
+        }
+
+*/
